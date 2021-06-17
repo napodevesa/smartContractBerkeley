5cae3834cf3354a4a92e346eaee2885230974dae
napodevesa <napodevesa@gmail.com>
2021-06-17 00:07:43 -0300

    avances

--- curso dos/MyContract8.sol
+++ curso dos/MyContract8.sol
@@ -1,19 +1,25 @@
 pragma solidity 0.5.1;
 
-contract MyContract{
-
+contract ERC20Token {
+  string public name;
   mapping (address => uint256) public balances;
+
+  function mint() public {
+  balances [msg.sender] ++;
+
+  }
+
+}
+
+contract MyContract{
   address payable wallet;
-
-  event Purchase(
-    address indexed _buyer,
-    uint256 _amount
-    );
+  address token;
 
 
-  constructor (address payable _wallet) public
+  constructor (address payable _wallet, address _token) public
   {
     wallet = _wallet;
+    token = _token;
   }
 
 
@@ -24,8 +30,10 @@ contract MyContract{
 
   function buyToken() public payable
   {
-    balances [msg.sender] +=1;
+    ERC20Token _token = ERC20Token(address(token));
+    _token.mint();
     wallet.transfer(msg.value);
-    emit Purchase(msg.sender,1);
   }
+
+
 }
